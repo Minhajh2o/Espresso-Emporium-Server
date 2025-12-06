5ea0d587fd7fcc039acc9e362425bd72a5d87995
Minhajul Islam <mislam2330718@bscse.uiu.ac.bd>
2025-12-06 20:37:13 +0600

    cors origin applied and vercel.joson changed

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ky8ebme.mongodb.net/?appName=Cluster0`;
